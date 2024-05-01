4712f8e79d04a468096279c38da57e68311c64f3
kcprasiddha01 <kcprasiddha01@gmail.com>
2024-05-01 22:14:20 +0545

    Made some minor fixes to the footer to make it responsive

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -21,7 +21,7 @@ const Footer = () => {
         </div>
         <hr />
         <div className="footer-bottom">
-            <p className="footer-bottom-left">&#169; 2023 Prasiddha Jung K.C. All rights reserved.</p>
+            <p className="footer-bottom-left">&#169; 2024 Prasiddha Jung K.C. All rights reserved.</p>
             <div className="footer-bottom-right">
                 <p>Term of Services</p>
                 <p>Privacy Policy</p>
